70b86b617fa803fb417e741f36cc5b53182bfdee
StefanyVasc <stefany.vasc.sa@gmail.com>
2024-12-09 23:56:21 -0300

    add image

--- src/main/factories/decorators/AuthorizeHttpClientDecoratorFactory.ts
+++ src/main/factories/decorators/AuthorizeHttpClientDecoratorFactory.ts
@@ -1,13 +1,21 @@
-import { UserManager } from 'oidc-client';
+import { UserManager } from "oidc-client";
 
-import { HttpClient } from '@/data/protocols/http';
-import userManager from '@/infra/auth/user-manager';
-import { AuthorizeHttpClientDecorator } from '@/main/decorators';
-import { makeAxiosHttpClient } from '@/main/factories/http';
+import { HttpClient } from "@/data/protocols/http";
+import userManager from "@/infra/auth/user-manager";
+import { AuthorizeHttpClientDecorator } from "@/main/decorators";
+import { makeAxiosHttpClient } from "@/main/factories/http";
 
 export const makeAuthorizeHttpClientDecorator = (): HttpClient => {
   return new AuthorizeHttpClientDecorator(
     userManager as UserManager,
-    makeAxiosHttpClient(),
+    makeAxiosHttpClient()
   );
 };
+
+/* 
+
+ padrão Decorator, responsável por adicionar a funcionalidade de autorização 
+ (ou algum tipo de cabeçalho de autenticação) a um HttpClient existente, 
+ sem modificar diretamente a lógica de comunicação HTTP que ele já realiza.
+
+ */
